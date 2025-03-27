aded98841ce2a6ca96269e717b59ebe6903bd22e
Eduardo Almeida <eduardoeandroid@gmail.com>
2025-03-27 11:07:53 -0300

    Atualização

--- src/components/home/submenu/index.tsx
+++ src/components/home/submenu/index.tsx
@@ -1,14 +1,38 @@
+"use client";
+import { useState, useEffect } from "react";
+
 import Link from "next/link";
 import styles from "./styles.module.scss";
 import { X, Menu } from "lucide-react";
 
 export function Submenu() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth > 768) {
+        setIsOpen(false);
+      }
+    };
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  function toggleMenu() {
+    setIsOpen(!isOpen);
+  }
+
   return (
     <section className={styles.submenu}>
-      <div className={styles.submenuIcon}>
+      <div className={styles.submenuIcon} onClick={toggleMenu}>
         <Menu size={34} color="#121212" /> Menu
       </div>
-      <ul className={styles.ul}>
+      <ul className={`${styles.ul} ${isOpen ? styles.open : ""}`}>
+        {isOpen && (
+          <button onClick={toggleMenu}>
+            <X size={34} color="#121212" />
+          </button>
+        )}
         <li>
           <Link href="/post/pagina1">pagina 1</Link>
         </li>
